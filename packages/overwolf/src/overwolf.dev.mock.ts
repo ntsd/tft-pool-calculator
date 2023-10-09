869150e664f5448b000a7a289c6f7cd050e18e33
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-10-09 23:43:17 +0700

    feat: use overwolf screenshot and tesseract.js

--- packages/overwolf/src/overwolf.dev.mock.ts
+++ packages/overwolf/src/overwolf.dev.mock.ts
@@ -146,7 +146,7 @@ const overwolfMock: typeof overwolf = {
           success: true,
           activity: {
             aTime: 0,
-            apm: false,
+            apm: 0,
             iTime: 0,
             keyboard: {
               keys: [],
@@ -167,7 +167,7 @@ const overwolfMock: typeof overwolf = {
           success: true,
           activity: {
             aTime: 0,
-            apm: false,
+            apm: 0,
             iTime: 0,
             keyboard: {
               keys: [],
@@ -188,7 +188,7 @@ const overwolfMock: typeof overwolf = {
           success: true,
           activity: {
             aTime: 0,
-            apm: false,
+            apm: 0,
             iTime: 0,
             keyboard: {
               keys: [],
