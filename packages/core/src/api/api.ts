1d30d505c5261db200a4a7e0d8b8ce4af08c126b
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-10-17 22:14:17 +0700

    fix: fetch data path

--- packages/core/src/api/api.ts
+++ packages/core/src/api/api.ts
@@ -2,11 +2,11 @@ import { setMutator } from "../const.js";
 import type { Champion, Trait } from "../types.js";
 
 export const getChampions: () => Promise<Champion[]> = async () => {
-  const championsResp = await fetch(`data/${setMutator}/champions.json`);
+  const championsResp = await fetch(`/data/${setMutator}/champions.json`);
   return championsResp.json();
 };
 
 export const getTraits: () => Promise<Trait[]> = async () => {
-  const traitsResp = await fetch(`data/${setMutator}/traits.json`);
+  const traitsResp = await fetch(`/data/${setMutator}/traits.json`);
   return traitsResp.json();
 };
